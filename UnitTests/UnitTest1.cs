5b4568b36b38c7495981dfd5f194365a6515c226
acodered <alexkondratru@gmail.com>
2024-03-26 11:22:35 +0100

    Add TestExampleOutput() and refactor TestNoItemFits()

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,4 +1,6 @@
 using ProblemPlecakowy;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics.Metrics;
 
 namespace Knapsack_test
 {
@@ -13,18 +15,42 @@ namespace Knapsack_test
 
             Item[] arr = new Item[N] {
                 new Item(0, 10, 1),
-                new Item(0, 10, 1),
-                new Item(0, 10, 1),
-                new Item(0, 10, 1),
-                new Item(0, 10, 1)
+                new Item(1, 10, 1),
+                new Item(2, 10, 1),
+                new Item(3, 10, 1),
+                new Item(4, 10, 1)
             };
 
-            Problem obj = new Problem(N, 1);
+            Problem problem = new Problem(N, 1);
 
-            obj.Items = arr;
+            problem.Items = arr;
 
-            Assert.IsFalse(obj.Solve(cap).ItemsID.Any());
+            Assert.AreEqual(0, problem.Solve(cap).ItemsID.Count);
+        }
 
+        [TestMethod]
+        public void TestExampleOutput()
+        {
+
+            const int N = 5;
+            int cap = 15;
+
+            Item[] arr = new Item[N] {
+                new Item(0, 4, 9),
+                new Item(1, 3, 9),
+                new Item(2, 4, 9),
+                new Item(3, 8, 5),
+                new Item(4, 6, 3)
+            };
+
+            Problem problem = new Problem(N, 1);
+
+            problem.Items = arr;
+
+            var actual = problem.Solve(cap).ItemsID;
+
+            List<int> expected = new List<int> {1, 0, 2};
+            CollectionAssert.AreEqual(expected, actual);
         }
 
 
