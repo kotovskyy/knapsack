eaf297ffdac5933916633c936368d8a3eab8f1c9
acodered <alexkondratru@gmail.com>
2024-03-26 01:13:36 +0100

    Move ProblemPlecakowy to separate directory

---
+++ ProblemPlecakowy/Item.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using ProblemPlecakowy;
+
+[assembly: InternalsVisibleTo("UnitTests")]
+namespace ProblemPlecakowy
+{
+    internal class Item
+    {
+        private int id, waga, wartosc;
+        public int Id {
+            get { return id; } 
+            set { id = value; }
+        }
+        public int Waga
+        {
+            get { return waga; }
+            set { waga = value; }
+        }
+        public int Wartosc
+        {
+            get { return wartosc; }
+            set { wartosc = value; }
+        }
+
+        public Item(int id, int waga, int wartosc)
+        {
+            this.id = id;
+            this.waga = waga;
+            this.wartosc = wartosc;
+        }
+
+        public override string ToString()
+        {
+            return $"No.: {this.id} | v: {this.wartosc} | w: {this.waga}";
+        }
+    }
+}
---
+++ ProblemPlecakowy/Result.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using ProblemPlecakowy;
+
+namespace ProblemPlecakowy
+{
+    internal class Result
+    {
+        private List<int> itemsID;
+
+        public List<int> ItemsID
+        {
+            get { return itemsID; }
+        }
+
+        private int totalW;
+        private int totalV;
+        public int TotalW { get; set; }
+        public int TotalV { get; set; }
+
+        public Result(List<int> itemsID, int totalW, int totalV)
+        {
+            this.totalV = totalV;
+            this.totalW = totalW;
+            this.itemsID = itemsID;
+        }
+
+        public override string ToString()
+        {
+            string output = "items: ";
+            foreach(var id in this.itemsID) {
+                output += id + " ";
+            }
+            output += Environment.NewLine + $"total value: {this.totalV}";
+            output += Environment.NewLine + $"total weight: {this.totalW}";
+
+            return output;
+        }
+    }
+}
